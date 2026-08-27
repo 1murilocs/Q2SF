b419c141cce45e612c4dd0dc8b2bdbb92696681d
Murilo <neckray50@gmail.com>
2026-08-27 10:43:20 -0300

    Q2SF V12 - FINAL

--- Q2SF/script_queries/query_quiver.sql
+++ Q2SF/script_queries/query_quiver.sql
@@ -1,12 +1,12 @@
 select
 	td.Apolice as Numero_da_Apolice__c,
-	left(td.Proposta, 6) as Proposta__c,
-	datefromparts(year(td.Data_emissao), month(td.Data_emissao), day(td.Data_emissao)) AS Data_de_Emissao__c,
-    datefromparts(year(td.Inicio_vigencia), month(td.Inicio_vigencia), day(td.Inicio_vigencia )) as Inicio_da_Vigencia__c,
-	datefromparts(year(td.Termino_vigencia), month(td.Termino_vigencia), day(td.Termino_vigencia)) as Termino_da_Vigencia__c,
-	td.Situacao as Status__c,
-	left(td.Proposta_cia, 12) as Numero_da_Oportunidade__c,
-	td.Premio_total as Premio_Quiver__c
+	min(left(td.Proposta, 6)) as Proposta__c,
+	min(datefromparts(year(td.Data_emissao), month(td.Data_emissao), day(td.Data_emissao))) AS Data_de_Emissao__c,
+    min(datefromparts(year(td.Inicio_vigencia), month(td.Inicio_vigencia), day(td.Inicio_vigencia ))) as Inicio_da_Vigencia__c,
+	min(datefromparts(year(td.Termino_vigencia), month(td.Termino_vigencia), day(td.Termino_vigencia))) as Termino_da_Vigencia__c,
+	min(td.Situacao) as Status__c,
+	min(left(td.Proposta_cia, 12)) as Numero_da_Oportunidade__c,
+	min(td.Premio_total) as Premio_Quiver__c
 from Tabela_Documentos td
 join Tabela_Clientes tc
 	on tc.Cliente = td.Cliente
@@ -25,4 +25,6 @@ and td.Termino_vigencia is not null
 and td.Proposta_cia is not null
 and trim(td.Endosso) = ''
 and td.Proposta_cia like 'opo-%'
-order by td.Inicio_vigencia desc;
+group by td.Apolice
+having count(*) >= 1
+order by min(td.Inicio_vigencia) desc;
--- Q2SF/script_queries/query_quiver_update.sql
+++ Q2SF/script_queries/query_quiver_update.sql
@@ -1,12 +1,12 @@
 select
 	td.Apolice as Numero_da_Apolice__c,
-	left(td.Proposta, 6) as Proposta__c,
-	datefromparts(year(td.Data_emissao), month(td.Data_emissao), day(td.Data_emissao)) AS Data_de_Emissao__c,
-    datefromparts(year(td.Inicio_vigencia), month(td.Inicio_vigencia), day(td.Inicio_vigencia )) as Inicio_da_Vigencia__c,
-	datefromparts(year(td.Termino_vigencia), month(td.Termino_vigencia), day(td.Termino_vigencia)) as Termino_da_Vigencia__c,
-	td.Situacao as Status__c,
-	td.Proposta_cia as Numero_da_Oportunidade__c,
-	td.Premio_total as Premio_Quiver__c
+	min(left(td.Proposta, 6)) as Proposta__c,
+	min(datefromparts(year(td.Data_emissao), month(td.Data_emissao), day(td.Data_emissao))) AS Data_de_Emissao__c,
+    min(datefromparts(year(td.Inicio_vigencia), month(td.Inicio_vigencia), day(td.Inicio_vigencia ))) as Inicio_da_Vigencia__c,
+	min(datefromparts(year(td.Termino_vigencia), month(td.Termino_vigencia), day(td.Termino_vigencia))) as Termino_da_Vigencia__c,
+	min(td.Situacao) as Status__c,
+	min(left(td.Proposta_cia, 12)) as Numero_da_Oportunidade__c,
+	min(td.Premio_total) as Premio_Quiver__c
 from Tabela_Documentos td
 join Tabela_Clientes tc
 	on tc.Cliente = td.Cliente
@@ -15,12 +15,16 @@ join Tabela_Produtos tp
 join Tabela_Seguradoras ts
 	on td.Seguradora = ts.Seguradora
 where year(td.Inicio_vigencia) = year(getdate()) 
-and td.Inicio_vigencia >= dateadd(day, -30, cast(getdate() as date))
+and td.Inicio_vigencia >= dateadd(day, -45, cast(getdate() as date))
 and td.Inicio_vigencia <= cast(getdate() as date)
 and td.Apolice is not null
 and td.Proposta is not null
 and td.Data_emissao is not null
 and td.Inicio_vigencia is not null
 and td.Termino_vigencia is not null
-and td.Proposta_cia like '%opo%'
-order by td.Inicio_vigencia desc;
+and td.Proposta_cia is not null
+and trim(td.Endosso) = ''
+and td.Proposta_cia like 'opo-%'
+group by td.Apolice
+having count(*) >= 1
+order by min(td.Inicio_vigencia) desc;
