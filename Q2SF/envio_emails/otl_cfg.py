924a2feb50ce4c7fb12ddc317dc159497df5ec76
Murilo <neckray50@gmail.com>
2026-08-31 10:32:08 -0300

    Q2SF - V13 FINAL

---
+++ Q2SF/envio_emails/otl_cfg.py
@@ -0,0 +1,6 @@
+import os
+
+PASSWORD = 'Glc@2006' #senha e-mail galcorr
+RECEIVERS = ['marcos.yamaguti@galcorr.com.br', 'murilo.santos@galcorr.com.br']
+EMAIL = 'murilo.santos@galcorr.com.br' #e-mail galcorr
+DISTAP = []
---
+++ Q2SF/python/cfg.py
@@ -0,0 +1,10 @@
+import os
+
+SFUSER = 'admin_galcorr@galcorr.com.br' #usuario salesforce
+SFPASSWORD = 'F6mCorretor@' #senha salesforce
+SFTOKEN = 'egmjhpDpSCyxHGJLU9UvnnMO' #token salesforce
+
+SERVER = '192.168.0.5' #server do banco de dados
+DATABASE = 'colWeb' #banco 
+UID = 'sa' #usuário 
+DBPASS = 'glc5544' #senha 
